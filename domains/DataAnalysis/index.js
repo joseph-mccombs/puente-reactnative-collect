beb9f4829e765f93835984547acbea31911f2edc
Joseph McCombs <jamccomb92@gmail.com>
2020-09-08 19:08:33 -0400

    inital commit

--- domains/DataAnalysis/index.js
+++ domains/DataAnalysis/index.js
@@ -4,11 +4,13 @@ import {
 } from 'react-native';
 
 import theme from '../../modules/theme';
+import ImagePickerExample from '../../components/Multimedia/Photo/CameraRoll'
 
 export default function DataAnalysis() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Welcome to the data analysis page.</Text>
+      <ImagePickerExample />
     </View>
   );
 }

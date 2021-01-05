3fc0d906402fc71f42b94a805adef4490c266cee
Joseph McCombs <jamccomb92@gmail.com>
2021-01-04 22:15:02 -0500

    fix: remove parse information before submitting offline forms

--- modules/cached-resources/error-handling.js
+++ modules/cached-resources/error-handling.js
@@ -1,19 +1,25 @@
 import { Parse } from 'parse/react-native';
 import { retrieveSignInFunction } from '../../services/parse/auth';
-import { getData } from '../async-storage';
+import { getData, deleteData, getAllData } from '../async-storage';
 
-export default async function handleParseError(err, functionToCall) {
+export default function handleParseError(err, functionToCall) {
   return new Promise((resolve, reject) => {
     if (err.code === Parse.Error.INVALID_SESSION_TOKEN) {
-      getData('credentials').then((user) => {
-        retrieveSignInFunction(user.username, user.password).then(() => {
-          functionToCall().then(() => {
-            resolve(true);
+      // delete local async parse informatin
+      deleteData("Parse/myAppId/currentUser").then(() => {
+        // get locally stored user information if credentials are saved
+        getData('credentials').then((user) => {
+          // sign in the user to retrieve fresh session token
+          retrieveSignInFunction(user.username, user.password).then(() => {
+            // resubmit offline forms
+            functionToCall().then(() => {
+              resolve(true);
+            });
+          }, (error) => {
+            reject(error);
           });
-        }, (error) => {
-          reject(error);
         });
-      });
+      })
     }
   });
 }

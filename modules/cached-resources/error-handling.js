ed44f368a88bcc6bb27ff5ddd5a557096a3f8e13
Joseph McCombs <jamccomb92@gmail.com>
2021-01-04 22:18:00 -0500

    chore: linting

--- modules/cached-resources/error-handling.js
+++ modules/cached-resources/error-handling.js
@@ -1,12 +1,12 @@
 import { Parse } from 'parse/react-native';
 import { retrieveSignInFunction } from '../../services/parse/auth';
-import { getData, deleteData, getAllData } from '../async-storage';
+import { getData, deleteData } from '../async-storage';
 
 export default function handleParseError(err, functionToCall) {
   return new Promise((resolve, reject) => {
     if (err.code === Parse.Error.INVALID_SESSION_TOKEN) {
       // delete local async parse informatin
-      deleteData("Parse/myAppId/currentUser").then(() => {
+      deleteData('Parse/myAppId/currentUser').then(() => {
         // get locally stored user information if credentials are saved
         getData('credentials').then((user) => {
           // sign in the user to retrieve fresh session token
@@ -19,7 +19,7 @@ export default function handleParseError(err, functionToCall) {
             reject(error);
           });
         });
-      })
+      });
     }
   });
 }
